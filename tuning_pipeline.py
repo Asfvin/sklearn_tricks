f2e929f32bd3edd55243b68223469eb151282e8a
Asfvin Pugunisparam <asfvin.p@gmail.com>
2023-09-24 22:20:58 +0800

    Include initial pipeline transformation

---
+++ tuning_pipeline.py
@@ -0,0 +1,40 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.compose import make_column_transformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+
+
+df = pd.DataFrame({
+    'Fare': [1, 2, 3, 4, 5, 1, 2, 3, 4, 5], 
+    'Embarked': [1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+    'Name': ['Ash', 'Brock', 'Misty', 'Oak', 'Meme', 'Ash', 'Brock', 'Misty', 'Oak', 'Meme'],
+    'Sex': ['male', 'female', 'male', 'female', 'female', 'male', 'female', 'male', 'female', 'female'],
+    'Age': [10, 20, 23, 40, 30, 10, 20, 23, 40, 30],
+    })
+
+X = df.drop(columns=['Embarked'])
+y = df['Embarked']
+
+ohe = OneHotEncoder()
+vect = CountVectorizer()
+ct = make_column_transformer((ohe, ['Sex']), (vect, 'Name'))
+clf = LogisticRegression(solver='liblinear', random_state=1)
+
+
+pipe = make_pipeline(ct, clf)
+cross_val_score(pipe, X, y, cv=5, scoring='accuracy').mean()
+
+params = {}
+params['columntransformer__countvectorizer__min_df'] = [1, 2]
+params['logisticregression__C'] = [0.1, 1, 10]
+params['logisticregression__penalty'] = ['l1', 'l2']
+
+grid = GridSearchCV(pipe, params, cv=5, scoring='accuracy')
+
+grid.fit(X, y)
+
+grid.best_score_
